26d2c208ecc2108e46fe7db756014759965f782b
ZhuckovAlex <Aleksander.zhuckov2011@gmail.com>
2025-07-22 10:03:20 +0300

    Update ver

--- src/main/java/ru/imaginaerum/wd/common/items/custom/MagicHat.java
+++ src/main/java/ru/imaginaerum/wd/common/items/custom/MagicHat.java
@@ -1,5 +1,8 @@
 package ru.imaginaerum.wd.common.items.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.monster.EnderMan;
@@ -7,6 +10,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import ru.imaginaerum.wd.WD;
 import ru.imaginaerum.wd.server.item.CustomArmorPostRender;
@@ -14,10 +19,21 @@ import ru.imaginaerum.wd.server.item.KeybindUsingArmor;
 import ru.imaginaerum.wd.server.item.UpdatesStackTags;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class MagicHat extends ArmorItem implements CustomArmorPostRender, KeybindUsingArmor, UpdatesStackTags {
     public MagicHat(ArmorMaterial armorMaterial, Type type, Properties properties) {
         super(armorMaterial, type, properties);
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if(Screen.hasShiftDown()) {
+            components.add(Component.translatable("wd.press_shift2").withStyle(ChatFormatting.DARK_GRAY));
+            components.add(Component.translatable("wd.hat_helmet_clear").withStyle(ChatFormatting.DARK_PURPLE));
+        } else {
+            components.add(Component.translatable("wd.press_shift").withStyle(ChatFormatting.DARK_GRAY));
+        }
+        super.appendHoverText(stack, level, components, flag);
     }
     @Override
     public void initializeClient(java.util.function.Consumer<IClientItemExtensions> consumer) {
--- src/main/java/ru/imaginaerum/wd/common/items/custom/MagicHatJam.java
+++ src/main/java/ru/imaginaerum/wd/common/items/custom/MagicHatJam.java
@@ -1,5 +1,8 @@
 package ru.imaginaerum.wd.common.items.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -8,6 +11,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import ru.imaginaerum.wd.WD;
 import ru.imaginaerum.wd.server.item.CustomArmorPostRender;
@@ -15,6 +20,7 @@ import ru.imaginaerum.wd.server.item.KeybindUsingArmor;
 import ru.imaginaerum.wd.server.item.UpdatesStackTags;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class MagicHatJam extends ArmorItem implements CustomArmorPostRender, KeybindUsingArmor, UpdatesStackTags {
     public MagicHatJam(ArmorMaterial armorMaterial, Type type, Properties properties) {
@@ -23,6 +29,16 @@ public class MagicHatJam extends ArmorItem implements CustomArmorPostRender, Key
     @Override
     public void initializeClient(java.util.function.Consumer<IClientItemExtensions> consumer) {
         consumer.accept((IClientItemExtensions) WD.PROXY.getArmorProperties());
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if(Screen.hasShiftDown()) {
+            components.add(Component.translatable("wd.press_shift2").withStyle(ChatFormatting.DARK_GRAY));
+            components.add(Component.translatable("wd.hat_helmet").withStyle(ChatFormatting.DARK_PURPLE));
+        } else {
+            components.add(Component.translatable("wd.press_shift").withStyle(ChatFormatting.DARK_GRAY));
+        }
+        super.appendHoverText(stack, level, components, flag);
     }
     @Nullable
     public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
