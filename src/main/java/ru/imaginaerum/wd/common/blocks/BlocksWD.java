a91b2f30a2b090469220c96482e7513480995f8d
Vadim Olennikov <VDOlennikov@alfabank.ru>
2025-10-20 10:50:44 +0300

    Edit comment for jams

--- src/main/java/ru/imaginaerum/wd/common/blocks/BlocksWD.java
+++ src/main/java/ru/imaginaerum/wd/common/blocks/BlocksWD.java
@@ -21,7 +21,7 @@ import java.util.function.Supplier;
 public class BlocksWD {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, WD.MODID);
-    // Джемы
+    // Волшебные джемы
     public static final RegistryObject<Block> SWEET_JAM = BLOCKS.register("sweet_jam",
             () -> new SweetJamBlock(BlockBehaviour.Properties.of().strength(0.2F).sound(SoundType.GLASS).noOcclusion(), ItemsWD.SWEET_JAM));
     public static final RegistryObject<Block> APPLE_JAM = BLOCKS.register("apple_jam",
